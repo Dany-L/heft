6fd933f6714297728bdc813630a09a93c63c2158
dany-l-1992 <daniel@familiefrank.eu>
2019-08-22 23:38:19 +0200

    init save to xml and identify frames

---
+++ saveToXml.jsx
@@ -0,0 +1,13 @@
+﻿// get file by name
+var fileName = "heft_19_20_script_test.indd";
+
+var myDoc = app.documents.itemByName (fileName);
+var TextFrames = myDoc.pages.item(1).textFrames;
+var numFrames= TextFrames.length;
+
+var frames;
+for (frames = 0; frames < numFrames; frames++) {
+    frame = TextFrames.item(frames);
+    frame.contents = "";
+    frame.contents = "nr" + frames.toString();
+}
